4dfa45ba3cb13d343aa8b5fde27e07124e1921f2
Larry Zhao <245313315@qq.com>
2018-02-06 10:37:21 +0000

    Half done Film Rental

--- Template.Model/Rental.cs
+++ Template.Model/Rental.cs
@@ -5,9 +5,11 @@ namespace Template.Model
 {
     public class Rental
 	{
+		[NakedObjectsIgnore]
 		virtual public int RentalID { get; set; }
-		virtual public int FilmOfRental { get; set; }
-		virtual public int CustomerOfRental { get; set; } 
+
+		virtual public Film FilmOfRental { get; set; }
+		virtual public Customer CustomerOfRental { get; set; } 
 		virtual public DateTime DateOfRental { get; set; }
 		virtual public DateTime DateOfExpire { get; set; }
 	}
