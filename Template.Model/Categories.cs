bc73fa67b024bde3aa5e7b2b9c897be70c35120b
Kamsi Mbanefo <Kamsimbanefo@yahoo.com>
2018-02-06 11:17:15 +0000

    Default Categories Made

---
+++ Template.Model/Categories.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Template.Model
+{
+    public enum Categories
+    {
+        Action, Adventure, Comedy, Drama, Romance, Sci_Fi
+    }
+}
--- Template.Model/Film.cs
+++ Template.Model/Film.cs
@@ -14,12 +14,13 @@ namespace Template.Model
         [Title]
         public virtual string FilmName { get; set; }
 
-		public virtual string Category { get; set; }
+		public virtual Categories Category { get; set; }
 
 		public virtual string FilmMaker { get; set; }
 
 		public virtual DateTime YearOfRelease { get; set; }
 
+        [Mask("c")]
 		public virtual double Price { get; set; }
 
 		public virtual int AgeRating { get; set; }
